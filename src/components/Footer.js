02bf9463f7bc4b871f3c89440ec63b36277835a9
Jo Meenen <jo@meenen.de>
2018-02-11 18:04:41 +0100

    Add Footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import { h, app } from "hyperapp"
+
+export default ({ color }, children) => (
+  <footer class={`footer ${color}`}>
+		<div class="container">
+			<div class="content has-text-centered">
+				<p>{children}</p>
+			</div>
+		</div>
+	</footer>
+)
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,6 +1,7 @@
 import { h, app } from "hyperapp"
 import { Route } from "@hyperapp/router"
 import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
 import HomeView from './HomeView'
 import ComponentsView from './ComponentsView'
 import AppView from './AppView'
@@ -11,5 +12,8 @@ export default (state, actions) => (
     <Route path="/" render={HomeView} />
     <Route path="/components" render={ComponentsView} />
     <Route path="/app" render={AppView} />
+    <Footer>
+      <strong>HyperBulma</strong> by <a href="https://steemit.com/@cutemachine">@cutemachine</a>.
+    </Footer>
   </div>
 )
