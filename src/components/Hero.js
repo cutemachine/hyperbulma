809854c2474bfa3fc27d9be4498bd2907c7c1934
Jo Meenen <jo@meenen.de>
2018-02-11 18:07:22 +0100

    Add Hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,12 @@
+import { h, app } from "hyperapp"
+
+export default ({ color, title, subtitle }, children) => (
+  <section class={`hero is-${color}`}>
+		<div class='hero-body'>
+			<div class='container'>
+				<h1 class='title is-1'>{title}</h1>
+				<h2 class='subtitle is-2'>{subtitle}</h2>
+			</div>
+		</div>
+	</section>
+)
--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,6 +1,7 @@
 import { h, app } from "hyperapp"
 import { Route } from "@hyperapp/router"
 import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
 import Footer from '../components/Footer'
 import HomeView from './HomeView'
 import ComponentsView from './ComponentsView'
@@ -9,6 +10,7 @@ import AppView from './AppView'
 export default (state, actions) => (
   <div>
     <Navbar />
+    <Route path='/' render={() => <Hero color='primary' title='HyperBulma' subtitle='A Hyperapp Starter Using Bulma CSS' />} />
     <Route path="/" render={HomeView} />
     <Route path="/components" render={ComponentsView} />
     <Route path="/app" render={AppView} />
