803030d391672679a018c0c3d08f4899b8b64bb1
Jo Meenen <jo@meenen.de>
2018-02-11 18:47:25 +0100

    Add Bulma title samples

--- src/views/ComponentsView.js
+++ src/views/ComponentsView.js
@@ -5,8 +5,18 @@ export default () => (
     <div class="container">
       <h1 class="title">Components</h1>
       <h2 class="subtitle">
-        This is the Components view.
+        Showcase of some Bulma components.
       </h2>
+      <div class="block">
+        <h1 class="title">h1 title</h1>
+        <h2 class="subtitle">h2 subtitle</h2>
+        <h1 class="title is-1">title is-1</h1>
+        <h2 class="title is-2">title is-2</h2>
+        <h3 class="title is-3">title is-3</h3>
+        <h4 class="title is-4">title is-4</h4>
+        <h5 class="title is-5">title is-5</h5>
+        <h6 class="title is-6">title is-6</h6>
+      </div>
     </div>
   </section>
 )
